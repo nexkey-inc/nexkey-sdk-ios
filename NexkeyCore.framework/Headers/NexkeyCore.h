7097e5425d34aaa61c2994da869b29706804a45e
Lukasz Dworakowski <lukasz.dworakowski@nexkey.com>
2019-03-05 15:09:25 +0100

    Initial release

---
+++ NexkeyCore.framework/Headers/NexkeyCore.h
@@ -0,0 +1,18 @@
+//
+//  NexkeyCore.h
+//  NexkeyCore
+//
+//  Copyright © 2016 Nexkey, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NexkeyCore.
+FOUNDATION_EXPORT double NexkeyCoreVersionNumber;
+
+//! Project version string for NexkeyCore.
+FOUNDATION_EXPORT const unsigned char NexkeyCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NexkeyCore/PublicHeader.h>
+
+
